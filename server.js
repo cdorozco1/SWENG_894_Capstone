de49af09011312557c8c689ea05a66f32ef5564f
cdorozco1 <90574049+cdorozco1@users.noreply.github.com>
2021-11-07 18:25:38 -0600

    deployment edits

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/verifyToken', function (req, res) {
 });
 
 // Configure port number and listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
