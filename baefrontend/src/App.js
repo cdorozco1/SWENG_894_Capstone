9f163c4e99f4d3eeca85c832b4268c22ec5aa979
cdorozco1 <90574049+cdorozco1@users.noreply.github.com>
2021-11-07 18:35:03 -0600

    deployment edits

--- baefrontend/src/App.js
+++ baefrontend/src/App.js
@@ -24,26 +24,26 @@ import {getToken, removeUserSession, setUserSession} from './components/utilitie
 import './styling/styles.css'
 
 function App() {
-  const [authLoading, setAuthLoading] = useState(true);
-  // Get the token
-  useEffect (() =>{
-    const token = getToken();
-    if(!token) {
-      return;
-    }
+  // const [authLoading, setAuthLoading] = useState(true);
+  // // Get the token
+  // useEffect (() =>{
+  //   const token = getToken();
+  //   if(!token) {
+  //     return;
+  //   }
     
-    axios.get(`http://localhost:8080/verifyToken?token=${token}`).then(response => {
-      setUserSession(response.data.token, response.data.user);
-      setAuthLoading(false);
-    }).catch(error => {
-      removeUserSession();
-      setAuthLoading(false);
-    });
-  }, []);
+  //   axios.get(`http://localhost:8080/verifyToken?token=${token}`).then(response => {
+  //     setUserSession(response.data.token, response.data.user);
+  //     setAuthLoading(false);
+  //   }).catch(error => {
+  //     removeUserSession();
+  //     setAuthLoading(false);
+  //   });
+  // }, []);
 
-  if (authLoading && getToken()) {
-    return <div className = "content">Verifying Authentication...</div>
-  }
+  // if (authLoading && getToken()) {
+  //   return <div className = "content">Verifying Authentication...</div>
+  // }
 
   return (
     <div>
